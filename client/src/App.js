9664ddb6e77e1165410f47eea04798f60c739830
iwswordpress <iwswordpress@gmail.com>
2020-10-11 11:29:48 +0100

    onvert to bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
 
-import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 import MenuBar from './components/MenuBar';
@@ -12,14 +10,14 @@ import Register from './pages/Register';
 
 function App() {
   return (
-    <Router>
-      <Container>
+    <div class='container'>
+      <Router>
         <MenuBar />
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
-      </Container>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
